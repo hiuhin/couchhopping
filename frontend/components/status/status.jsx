d0d6ada1f48deda01932199137bfc68a3d751d2f
Anne Wong <hiuhin@gmail.com>
2020-02-28 23:29:33 -0800

    Remove Edit Profile Button for Non-CurrentUser

--- frontend/components/status/status.jsx
+++ frontend/components/status/status.jsx
@@ -9,11 +9,16 @@ class Status extends React.Component {
 
     render() {
         const { currentUser } = this.props;
+        console.log(currentUser);
+        console.log(this.props);
+        let display = currentUser.id === this.props.user.id ? <Link to={`/profile/${currentUser.id}/edit`}>Edit My Profile</Link> : "";
+
 
         return (
             <div className="status">
                 <h2>{currentUser.status}</h2>
-                <Link to={`/profile/${currentUser.id}/edit`}>Edit My Profile</Link>
+                {/* <Link to={`/profile/${currentUser.id}/edit`}>Edit My Profile</Link> */}
+                {display}
             </div>
         )
     }
