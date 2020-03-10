7bd593bae9342463e84c7fcb060ca519eb6a2de5
Anne <hiuhin@gmail.com>
2020-03-09 23:22:41 -0700

    Add DirectRequestItem

--- frontend/components/dashboard/DirectRequestItem.jsx
+++ frontend/components/dashboard/DirectRequestItem.jsx
@@ -1,42 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-class Todo extends React.Component {
+class DirectRequestItem extends React.Component {
     constructor(props) {
         super(props);
     }
 
-    componentDidMount() {
-        this.props.fetchDirectRequests();
-    }
-
     render() {
-
+        const {request} = this.props;
         return (
             <div>
-                <h2>TO-DO LIST</h2>
-                <p>REQUESTS TO STAY WITH ME</p>
-                <ul>
-                    {this.props.directrequests.map(request =>
-                        <li>
-                            <DirectRequestItem request={request} />
-                        </li>
-                    )}
-                </ul>
+                {request.message}
 
             </div>
         )
     }
 }
 
-const mSTP = ({ entities: { directRequests } }) => {
-    return {
-        directrequests: Object.values(directRequests)
-    };
-};
+export default DirectRequestItem;
 
-const mDTP = dispatch => ({
-    fetchDirectRequests: () => dispatch(fetchDirectRequests())
-})
-
-export default connect(mSTP, mDTP)(Todo);
