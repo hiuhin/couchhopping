1bd40b9bb74515e1d50e37db82881c2ea88eca31
Anne <hiuhin@gmail.com>
2020-04-05 16:23:02 -0700

    Add To-Do List Empty Message

--- frontend/components/dashboard/todo.jsx
+++ frontend/components/dashboard/todo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
 import { fetchDirectRequests} from '../../actions/direct_request_actions';
 import DirectRequestItem from '../direct_requests/direct_request_item';
 
@@ -11,15 +12,20 @@ class Todo extends React.Component {
     componentDidMount() {
         this.props.fetchDirectRequests();
     }
+
+    renderNoRequests(){
+        return <p className="mod">No Pending Requests. Check <Link to={"/inbox"}>Inbox</Link> For Completed Requests.</p>
+    }
     
     render() {
         const {directrequests, session} = this.props;
         let requestsToUser = directrequests.filter(directrequest => directrequest.host_id === session.id && directrequest.response === "")
-
+      
         return (
             <div>
                 <h3 className="box-header">📝 TO-DO LIST</h3>
                 <p className="box-title">Requests To Stay With Me</p>
+                {requestsToUser.length === 0 ? this.renderNoRequests() : ""}
                 <ul>
                     {requestsToUser.map(requestToUser => 
                         <li key={requestToUser.id}>
