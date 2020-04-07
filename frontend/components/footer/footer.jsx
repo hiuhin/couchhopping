3b7b5e236b6178e2d554e569c753877b9ee1ae0a
Anne <hiuhin@gmail.com>
2020-04-07 14:42:16 -0700

    Add Footer Links

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,15 +3,20 @@ import React, { Component } from 'react'
 export class Footer extends Component {
     render() {
         return (
-          <div className="container">
-              <div className="footer">
+          <div className="footer-container">
+            <div className="footer">
+              <div>
                 <header>CONNECT WITH ME</header>
                 <ul className="footer-icons">
-                <i className="fab fa-github" />
-                <i className="fab fa-linkedin-in"></i>
-                <i className="fab fa-angellist"></i>
+                    <a href="https://github.com/hiuhin" target="_blank">
+                        <i className="fab fa-github" /></a>
+                    <a href="https://www.linkedin.com/in/anne-wong-231b0417a/" target="_blank">
+                        <i className="fab fa-linkedin-in"></i></a>
+                    <a href="https://angel.co/u/anne-wong-6" target="_blank"><i className="fab fa-angellist"></i></a>
                 </ul>
               </div>
+              <div>© 2020 Clone of CouchSurfing.com by Hiu Hin Anne Wong</div>
+            </div>
           </div>
         );
     }
